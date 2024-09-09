e2c808b5abac1de3da96fa2cef1ef24eec5c9045
Michael Hobbs <m.hobbs@deakin.edu.au>
2024-09-09 19:31:43 +1000

    Trying to fix improper tf file 4

--- main.tf
+++ main.tf
@@ -38,26 +38,26 @@ resource "azurerm_container_registry" "container_registry" {
   sku                 = "Basic"
 }
 
-resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = var.app_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg-aks.name
-  dns_prefix          = var.app_name
-  kubernetes_version  = var.kubernetes_version
+# resource "azurerm_kubernetes_cluster" "cluster" {
+#   name                = var.app_name
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg-aks.name
+#   dns_prefix          = var.app_name
+#   kubernetes_version  = var.kubernetes_version
 
-  default_node_pool {
-      name            = "default"
-      node_count      = 1
-      vm_size         = "Standard_B2s"
-  }
+#   default_node_pool {
+#       name            = "default"
+#       node_count      = 1
+#       vm_size         = "Standard_B2s"
+#   }
 
-  #
-  # Instead of creating a service principle have the system figure this out.
-  #
-  identity {
-      type = "SystemAssigned"
-  }    
-}
+#   #
+#   # Instead of creating a service principle have the system figure this out.
+#   #
+#   identity {
+#       type = "SystemAssigned"
+#   }    
+# }
 
 # #
 # # Attaches the container registry to the cluster.
